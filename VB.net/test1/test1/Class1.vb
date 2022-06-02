2c35fdc0b446678210e66bd4e59ea607b7004e15
chanasorn1234 <62010153@kmitl.ac.th>
2022-06-02 15:57:59 +0700

    data_struc

--- VB.net/test1/test1/Class1.vb
+++ VB.net/test1/test1/Class1.vb
@@ -1,9 +1,30 @@
 ﻿Public Class Class1
-    Dim p As String
+    Dim queue As New List(Of String)()
+    Dim count As Integer
+    Dim peek As Integer
+
     Public Sub New()
-        p = "chanasorn"
+        count = 0
+        peek = 0
     End Sub
-    Public Sub display()
-        Console.WriteLine(p)
+
+    Public Sub push(i)
+        queue.Add(i)
+        count += 1
     End Sub
+
+    Public Function pop()
+        If (count! = 0) Then
+            count -= 1
+            peek = queue(0)
+            queue.RemoveAt(0)
+            Return peek
+        Else
+            Return "Empty Queue"
+        End If
+    End Function
+
+    Public Function sizeOfqueue()
+        Return count
+    End Function
 End Class
--- VB.net/test1/test1/Program.vb
+++ VB.net/test1/test1/Program.vb
@@ -2,24 +2,10 @@ Imports System
 Imports Class1
 Module Program
     Dim x As Integer
-    Dim obj As New Class1()
+    Dim q As New Class1()
+
     Sub Main(args As String())
-        x = Console.ReadLine()
-        For i As Integer = 0 To x
-            For j As Integer = 0 To i
-                Console.Write("*")
-            Next
-            Console.WriteLine()
-        Next
-        obj.display()
-        If (x = 10) Then
-            Console.WriteLine("equal 10")
-        Else
-            Console.WriteLine("No")
-        End If
-        While (x!= 0)
-            x -= 1
-        End While
-        Console.WriteLine("x = 0 done")
+        q.push(5)
+        Console.WriteLine(q.sizeOfqueue())
     End Sub
 End Module
