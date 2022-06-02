fc4442c54e54ce0b978619aadeaf63acb2d18da5
chanasorn1234 <62010153@kmitl.ac.th>
2022-06-02 17:14:30 +0700

    stack

---
+++ VB.net/test1/test1/Class2.vb
@@ -0,0 +1,34 @@
+﻿Public Class Class2
+    Dim stack As New List(Of String)()
+    Dim count As Integer
+    Dim peek As Integer
+    Public Sub New()
+        count = 0
+        peek = 0
+    End Sub
+    Public Sub push(i)
+        stack.Add(i)
+        count += 1
+    End Sub
+    Public Function pop()
+        If (count <> 0) Then
+            peek = stack(count - 1)
+            stack.RemoveAt(count - 1)
+            count -= 1
+            Return peek
+        Else
+            Return "Empty Stack"
+        End If
+    End Function
+    Public Function sizeOfstack()
+        Return count
+    End Function
+    Public Function peekOfstack()
+        If (count <> 0) Then
+            Return stack(count - 1)
+        Else
+            Return "Empty Stack"
+        End If
+    End Function
+
+End Class
--- VB.net/test1/test1/Program.vb
+++ VB.net/test1/test1/Program.vb
@@ -1,11 +1,16 @@
 Imports System
 Imports Class1
+Imports Class2
 Module Program
     Dim x As Integer
     Dim q As New Class1()
+    Dim s As New Class2()
 
     Sub Main(args As String())
         q.push(5)
+        s.push(5)
+        s.push(6)
         Console.WriteLine(q.sizeOfqueue())
+        Console.WriteLine(s.peekOfstack())
     End Sub
 End Module
